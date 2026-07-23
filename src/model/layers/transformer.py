b199e7bc2f4910e5ea01c7b4438ae9c3f792ee8a
Aadeesh Sharma <sharmaaadeesh@gmail.com>
2026-07-23 13:00:32 +0100

    refactor architecture to global average pooling and linformer attention

--- src/model/layers/transformer.py
+++ src/model/layers/transformer.py
@@ -1,5 +1,11 @@
 import keras
-from hgq.layers import QAdd, QBatchNormalization, QMultiHeadAttention, Quantizer
+from hgq.layers import (
+    QAdd,
+    QBatchNormalization,
+    QMultiHeadAttention,
+    QLinformerAttention,
+    Quantizer,
+)
 
 # Assuming you have imported your new functional definitions:
 # from .attention import apply_hgq_self_attention
@@ -11,12 +17,14 @@ def apply_hgq_transformer_block(
     in_dim,
     latent_dim=None,
     num_heads=1,
+    proj_dim_k=2,
     dropout=0.0,
     num_particles=30,
     activation="ReLU",
     normalization="Layer",
     momentum=0.9,
     quantize=True,
+    use_linformer=True,
     block_name="transformer_block",
     training=False,
 ):
@@ -62,13 +70,23 @@ def apply_hgq_transformer_block(
             bc=MinMax(1, 8),
         )
         with mha_scope:
-            attn_out = QMultiHeadAttention(
-                num_heads=num_heads,
-                key_dim=head_dim,
-                value_dim=head_dim,
-                output_shape=in_dim,
-                name=f"{block_name}_q_attention",
-            )(norm_x, norm_x, training=training)
+            if use_linformer:
+                attn_out = QLinformerAttention(
+                    num_heads=num_heads,
+                    key_dim=head_dim,
+                    value_dim=head_dim,
+                    proj_dim_k=proj_dim_k,
+                    output_shape=in_dim,
+                    name=f"{block_name}_linformer_attn",
+                )(norm_x, norm_x, training=training)
+            else:
+                attn_out = QMultiHeadAttention(
+                    num_heads=num_heads,
+                    key_dim=head_dim,
+                    value_dim=head_dim,
+                    output_shape=in_dim,
+                    name=f"{block_name}_q_attention",
+                )(norm_x, norm_x, training=training)
     else:
         attn_out = keras.layers.MultiHeadAttention(
             num_heads=num_heads,
